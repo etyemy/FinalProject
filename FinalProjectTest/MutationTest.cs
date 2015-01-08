0e2ae807205585995151be7b3015e42cefe34e18
motimonso <motimonso@gmail.com>
2015-01-08 16:54:28 +0200

    add Unit Test Class

---
+++ FinalProjectTest/MutationTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace FinalProjectTest
+{
+    [TestClass]
+    public class MutationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
