3e877021870503d03c86a054fdc2b505babc3caa
motimonso <motimonso@gmail.com>
2015-04-05 01:24:31 +0300

    Fix - Importent Files Added To Git

---
+++ FinalProject/Article.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinalProject
+{
+    public class Article
+    {
+        private string _title;
+        private string _author;
+        private int _year;
+        private string _journal;
+        private string _cosmicId;
+        private string _pubmedId;
+
+        public Article(string title, string author, int year, string journal, string cosmicId, string pubmedId)
+        {
+            _title = title;
+            _author = author;
+            _year = year;
+            _journal = journal.Split(';')[0].Trim(new char[] {'"'});
+            _cosmicId = cosmicId;
+            _pubmedId = pubmedId;
+        }
+        public override string ToString()
+        {
+            return "Title: " + _title + Environment.NewLine 
+                + "Author: " + _author + Environment.NewLine 
+                + "Year: " + _year + Environment.NewLine 
+                + "Journal: " + _journal + Environment.NewLine 
+                + "CosmicID: " + _cosmicId + Environment.NewLine 
+                + "PubmedID: " + _pubmedId + Environment.NewLine;
+        }
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return _author;
+            }
+        }
+        public string Year
+        {
+            get
+            {
+                return Convert.ToString(_year);
+            }
+        }
+        public string Journal
+        {
+            get
+            {
+                return _journal;
+            }
+        }
+        public string CosmicID
+        {
+            get
+            {
+                return _cosmicId;
+            }
+        }
+        public string PubMedID
+        {
+            get
+            {
+                return _pubmedId;
+            }
+        }
+
+    }
+}
---
+++ FinalProject/TSVHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public class TSVHandler
+    {
+        private List<Article> _allArticles;
+        enum TSVCol { Title = 0, Author, Year, Journal, Status, CosmicID, PubmedID };
+        public TSVHandler(string tsvString)
+        {
+            _allArticles = new List<Article>();
+            string[] lines = tsvString.Split('\n');
+            for(int i=1;i<lines.Length-1;i++)
+            {
+                string[] lineParts = lines[i].Split('\t');
+                string title = lineParts[(int)TSVCol.Title];
+                string author = lineParts[(int)TSVCol.Author];
+                int year = Convert.ToInt32(lineParts[(int)TSVCol.Year]);
+                string journal = lineParts[(int)TSVCol.Journal];
+                string cosmicId = lineParts[(int)TSVCol.CosmicID];
+                string pubmedId = lineParts[(int)TSVCol.PubmedID];
+                Article article = new Article(title, author, year, journal, cosmicId, pubmedId);
+                _allArticles.Add(article);
+            }
+        }
+        public List<Article> AllArticles
+        {
+            get
+            {
+                return new List<Article>(_allArticles);
+            }
+        }
+    }
+}
